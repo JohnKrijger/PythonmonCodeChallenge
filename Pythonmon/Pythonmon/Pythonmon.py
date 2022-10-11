6e8607a2472df0005511bc64f509efa0d6e51227
John Krijger <JohnKrijger23@gmail.com>
2022-10-11 20:22:11 +0200

    Add more pokémon

--- Pythonmon/Pythonmon/Pythonmon.py
+++ Pythonmon/Pythonmon/Pythonmon.py
@@ -3,19 +3,24 @@ from player import Player
 from battle import Battle
 
 def main():
+    all_pokemon = [
+        Pokemon("Lando-T", 642, 230, 100, 241),
+        Pokemon("Toxapex", 810, 116, 100, 106),
+        Pokemon("Heatran", 480, 298, 75, 278),
+        Pokemon("Kartana", 464, 253, 100, 348),
+        Pokemon("D-pult", 356, 255, 100, 421),
+        Pokemon("Rilla", 443, 321, 100, 269),
+    ]
+
     player_a = Player(
         "Trainer A",
-        Pokemon("Lando-T", 642, 230, 100, 241),
-        [
-            Pokemon("Toxapex", 810, 116, 100, 106),
-        ]
+        all_pokemon[0],
+        all_pokemon[1:]
     )
     player_b = Player(
         "Trainer B",
-        Pokemon("Lando-T", 642, 230, 100, 241),
-        [
-            Pokemon("Toxapex", 810, 116, 100, 106),
-        ]
+        all_pokemon[0],
+        all_pokemon[1:]
     )
     battle = Battle(player_a, player_b)
     battle.start()
