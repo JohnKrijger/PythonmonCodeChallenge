bb7390ad441dd13944cdbb569bb4e45d79d5d904
John Krijger <JohnKrijger23@gmail.com>
2022-10-11 17:14:02 +0200

    Add and test basic attack methods

--- Pythonmon/Pythonmon/Pythonmon.py
+++ Pythonmon/Pythonmon/Pythonmon.py
@@ -1,8 +1,23 @@
 from pokemon import Pokemon
 
 def main():
-    mon = Pokemon("Lando-T", 100, 100, 100, 100)
-    print(mon.name)
+    print("           [pokémon | HP/max|dmg|acc|spd]")
+    attacking_mon = Pokemon("Lando-T", 642, 230, 100, 241)
+    print(f"Attacking: [{attacking_mon.description()}]")
+    defending_mon = Pokemon("Toxapex", 810, 116, 100, 106)
+    print(f"Defending: [{defending_mon.description()}]")
+    damage = attacking_mon.attack()
+    if damage > 0:
+        print(f"{attacking_mon.name} dealt {damage} damage!")
+        health_lost = defending_mon.take_damage(damage)
+        print(f"{defending_mon.name} lost {health_lost}% HP!")
+        if not defending_mon.is_alive():
+            print(f"{defending_mon.name} fainted!")
+    else:
+        print(f"{attacking_mon.name} missed!")
+    print("           [pokémon | HP/max|dmg|acc|spd]")
+    print(f"Attacking: [{attacking_mon.description()}]")
+    print(f"Defending: [{defending_mon.description()}]")
 
 if __name__ == "__main__":
     main()
--- Pythonmon/Pythonmon/pokemon.py
+++ Pythonmon/Pythonmon/pokemon.py
@@ -1,11 +1,40 @@
+from random import randint
+
 class Pokemon:
     
-    def __init__(self, name, health, damage, hit_chance, speed):
+    def __init__(self, name, max_health, damage, hit_chance, speed):
         self.name = name # Name of the pokémon
-        self.health = health # Amount of damage the pokémon can take
+        self.max_health = max_health # Amount of damage the pokémon can take
+        self.health = max_health # Current health
         self.damage = damage # Damage of an attack
         self.hit_chance = hit_chance # Percentage chance to hit
         self.speed = speed # Determines attack order
+    
+
+    def attack(self):
+        """Returns the damage from an attack or 0 if it misses."""
+        return self.damage if randint(1, 100) <= self.hit_chance else 0
+
+    def take_damage(self, damage):
+        """
+        Reduce health by given damage amount, returns percentage of max health
+        lost.
+        """
+        old_health = self.health
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+        return (old_health - self.health) * 100 // self.max_health
+
+    def remaining_health_ratio(self):
+        """Returns the ratio of health remaining"""
+        return self.health / self.max_health
+
+    def is_alive(self):
+        return self.health >= 0
+
+    def description(self):
+        return f"{self.name:8}|{self.health:3}/{self.max_health:3}|{self.damage:3}|{self.hit_chance:3}|{self.speed}"
 
 
 
