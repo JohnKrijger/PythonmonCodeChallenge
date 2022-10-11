bc61c31ffab2fe75222c4972303c0c71351b57e2
John Krijger <JohnKrijger23@gmail.com>
2022-10-11 16:10:48 +0200

    Add Pokémon class

--- Pythonmon/Pythonmon/Pythonmon.py
+++ Pythonmon/Pythonmon/Pythonmon.py
@@ -1,5 +1,8 @@
+from pokemon import Pokemon
+
 def main():
-    print("Hello world!")
+    mon = Pokemon("Lando-T", 100, 100, 100, 100)
+    print(mon.name)
 
 if __name__ == "__main__":
     main()
---
+++ Pythonmon/Pythonmon/pokemon.py
@@ -0,0 +1,11 @@
+class Pokemon:
+    
+    def __init__(self, name, health, damage, hit_chance, speed):
+        self.name = name # Name of the pokémon
+        self.health = health # Amount of damage the pokémon can take
+        self.damage = damage # Damage of an attack
+        self.hit_chance = hit_chance # Percentage chance to hit
+        self.speed = speed # Determines attack order
+
+
+
